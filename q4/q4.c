5e5e2d8f239afa685b4a6e33617b23cecb30eb50
Amey Patel <ameypatel88@gmail.com>
2026-04-12 19:04:13 +0530

    Implement dynamic loading of shared libraries

---
+++ q4/q4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<dlfcn.h>
+
+int main()
+{
+    char o[10];
+    int a,b;
+    while(scanf(" %s %d %d",o,&a,&b)==3)
+    {
+        char libname[20];
+        strcpy(libname,"lib");
+        strcat(libname,o);
+        strcat(libname,".so");
+
+        void *pntr=dlopen(libname,RTLD_LAZY);
+        if(pntr==NULL)
+        {
+            printf("Error!! %s\n",dlerror());
+            continue;
+        }
+
+        int (*func)(int,int);//function pointer initialization
+        func=(int(*)(int,int))dlsym(pntr,o);//finding the function in the symbol table
+        if(func==NULL)
+        {
+            printf("Error!! %s\n",dlerror());
+            dlclose(pntr);
+            continue;
+        }
+
+        int ans=func(a,b);
+        printf("%d\n",ans);
+        dlclose(pntr);
+    }
+}
